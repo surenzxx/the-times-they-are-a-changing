be8dfa481cff619c3506f4fc86d53d40c59bcb15
Surendra <persaudsurendra@gmail.com>
2016-07-18 18:03:21 -0400

    first commit

---
+++ help.js
@@ -0,0 +1,25 @@
+$("#nav-name").html(name);
+$("#name").html(name);
+$("#footer-name").html(name);
+$("#subheading").html(subheading);
+$("#school").html(school);
+$("#school-subheading").html(schoolSubheading);
+$("#career").html(career);
+$("#career-subheading").html(careerSubheading);
+$("#hobbies-subheading").html(hobbiesSubheading);
+//$("#section-one").css("background", color);
+$("#section-two").css("background", color);
+//$("#section-three").css("background", color);
+$("#section-four").css("background", color);
+$("#section-five").css("background", color);
+$(".navbar-inverse").css("background", color);
+$(".navbar-inverse").css("border-bottom-color", color);
+$("title").html(name+" - "+subheading)
+$("#main-image").css("background-image", "url("+mainImageURL+")");
+$("#school-image").css("background-image", "url("+schoolImageURL+")");
+$("#career-image").css("background-image", "url("+careerImageURL+")");
+$("#hobbies-image").css("background-image", "url("+hobbiesImageURL+")");
+
+
+// Person who you're talking to's name 
+$("#headline").html(headline);
---
+++ personalize.js
@@ -0,0 +1,64 @@
+//Personalization
+
+// headline of article
+
+var headline = "name" + " Gives a Generous Donation to All Star Code";
+
+// name of person 
+var founder = "Austin Carvey";
+
+//pic of founder
+var schoolImageURL = "https://lh5.googleusercontent.com/-69pgXW97y6o/AAAAAAAAAAI/AAAAAAAAAWg/43z6jsm8Mmo/photo.jpg"
+
+//customer
+var customer = "dancers";
+
+// pic of lots customers or lots of products
+var mainImageURL = "http://k.b5z.net/i/u/2179443/i/San_TA88_Bojango.jpg";
+
+// the product
+var product = "tap shoes";
+
+// pic of single customer or single product, or just less than main background
+var careerImageURL = "http://www.mastersofamericantapdance.com/data/images/lane_alexander.jpg";
+
+//business name
+var name = "TapsRUs";
+
+//slogan
+var subheading = "Tap your way to success!";
+
+// services your business provides
+var career = "Tap Shoes Supply Store";
+
+//famous person name
+var famousName = 'Beyonce';
+
+//famous person image
+var hobbiesImageURL = "http://userscontent2.emaze.com/images/e5890483-8e22-44ce-8684-49557bb9a5ba/3e4e1fc4-c90d-47e9-bb96-981d84778579.jpg";
+
+var color = colorPurple;
+//colorRed, colorOrange, colorYellow,
+//colorLightGreen, colorDarkGreen,
+//colorBlue, colorPurple, colorSilver
+
+// DO NOT CHANGE
+// DO NOT CHANGE
+// DO NOT CHANGE
+
+//founder
+var school = founder;
+
+var tweetBody = "I love supporting " + name + ", it's the best organization in the world! Plus the founder, " + founder +", is awesome.";
+
+subheading = subheading.capAllWord();
+
+//founder blurb
+var schoolSubheading = founder + " always imagined leveraging technology and business to serve " + customer + ". As Founder and CEO of " + name + ", and a lover of " + product + " and " + customer + ", " + founder + " is now at work imagining not just what " + product + " can be, but what they should be in order best support " + customer + ".";
+
+// blurb about the services they provide
+var careerSubheading = name + " was started because " + founder + " believes in getting quality " + product + " to " + customer + ", ensuring all " + customer + " get an equal chance at success. At " + name + " we believe that our success is about putting " + customer + " first, and becuase we listen and deliver on their needs, we will always outperform companies that don't truly care. ";
+
+
+// DONT PUT ANYTHING HERE!
+var hobbiesSubheading = '';
