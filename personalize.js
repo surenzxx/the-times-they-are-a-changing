8a608dc2b5c512b7c0702e7f2096617eae5e443e
Surendra <persaudsurendra@gmail.com>
2016-08-01 17:20:20 -0400

    updated js again

--- personalize.js
+++ personalize.js
@@ -2,23 +2,39 @@
 
 // name of person
 var fullName = "Robert Bonners";
+var firstName = "Robert"
 var lastName = "Bonners";
+
 //pic of person
 var benefitImageURL = "https://lh5.googleusercontent.com/-69pgXW97y6o/AAAAAAAAAAI/AAAAAAAAAWg/43z6jsm8Mmo/photo.jpg"
 
 //net worth of person
 var newWorth = "$10,000,000";
 
-// services your business provides
-var career = "Tap Shoes Supply Store";
+//movements/causes they support
+var movement1 = "";
+var movement2 = "";
+
+//activities
+var activity1 = "";
+var activity2 = "";
+var activity3 = "";
+
+//favorite food & drink
+var food = "";
+var drink = "";
+
+//area person lives in
+var area = "";
 
 // heading for benefitPhotoDescription
-var benefitHeading = name + "Wows at All Star Code Benefit"
+var benefitHeading = name + "Wows at All Star Code Benefit";
 
 //benefit pargraph one
-var benefitParagraph1 = "August 6th," + name + " was named the top benefactor of the All Star Code 2016 Benefit." + lastName + "had donated a grand total of over" + netWorth "." + "Hosted at Christina Lewis Halpern’s summer home in the East Hamptons," + name + "is well known for their participation in various movements, including" + movement1 + ",and" + movement2 + "."
+var benefitParagraph1 = "August 6th," + name + " was named the top benefactor of the All Star Code 2016 Benefit." + lastName + "had donated a grand total of over" + netWorth "." + "Hosted at Christina Lewis Halpern’s summer home in the East Hamptons," + name + "is well known for their participation in various movements, including" + movement1 + ",and" + movement2 + "." + "They have shown unwavering support, and have been a huge proponent of All Star Code since the beginning."
+
+//benefit paragraph two
+var benefitParagraph2 = "On the weekends and in their freetime," + name + "can be found" + activity1 + "They also enjoy" + activity2 + "and" + activity3 + "." + name + "enjoys" + food + "with a refreshing" + drink+ "." + name + "lives in" + area + ", which they enjoy living in because" + reason + "." + "One thing that many don't know about" + name + "is that" + interestingFact + "."
 
 // DONT PUT ANYTHING HERE!
 var hobbiesSubheading = '';
-
-subheading = subheading.capAllWord();
