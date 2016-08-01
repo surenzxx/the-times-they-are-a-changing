1a375628fa2a05678e5e83fe7486ed3f10c216db
Surendra <persaudsurendra@gmail.com>
2016-08-01 17:01:33 -0400

    changed js

--- personalize.js
+++ personalize.js
@@ -1,60 +1,24 @@
 //Personalization
 
 // name of person
-var name = "Robert Bonner";
-
-//pic of founder
+var fullName = "Robert Bonners";
+var lastName = "Bonners";
+//pic of person
 var benefitImageURL = "https://lh5.googleusercontent.com/-69pgXW97y6o/AAAAAAAAAAI/AAAAAAAAAWg/43z6jsm8Mmo/photo.jpg"
 
-//customer
-var customer = "dancers";
-
-// pic of lots customers or lots of products
-var mainImageURL = "http://k.b5z.net/i/u/2179443/i/San_TA88_Bojango.jpg";
-
-// the product
-var product = "tap shoes";
-
-// pic of single customer or single product, or just less than main background
-var careerImageURL = "http://www.mastersofamericantapdance.com/data/images/lane_alexander.jpg";
-
-//business name
-var name = "SlamJam";
-
-//slogan
-var subheading = "Tap your way to success!";
+//net worth of person
+var newWorth = "$10,000,000";
 
 // services your business provides
 var career = "Tap Shoes Supply Store";
 
-//famous person name
-var famousName = 'Beyonce';
+// heading for benefitPhotoDescription
+var benefitHeading = name + "Wows at All Star Code Benefit"
 
-//famous person image
-var hobbiesImageURL = "http://userscontent2.emaze.com/images/e5890483-8e22-44ce-8684-49557bb9a5ba/3e4e1fc4-c90d-47e9-bb96-981d84778579.jpg";
-
-var color = colorPurple;
-//colorRed, colorOrange, colorYellow,
-//colorLightGreen, colorDarkGreen,
-//colorBlue, colorPurple, colorSilver
-
-// DO NOT CHANGE
-// DO NOT CHANGE
-// DO NOT CHANGE
-
-//founder
-var school = founder;
-
-var tweetBody = "I love supporting " + name + ", it's the best organization in the world! Plus the founder, " + founder +", is awesome.";
-
-subheading = subheading.capAllWord();
-
-//founder blurb
-var schoolSubheading = founder + " always imagined leveraging technology and business to serve " + customer + ". As Founder and CEO of " + name + ", and a lover of " + product + " and " + customer + ", " + founder + " is now at work imagining not just what " + product + " can be, but what they should be in order best support " + customer + ".";
-
-// blurb about the services they provide
-var careerSubheading = name + " was started because " + founder + " believes in getting quality " + product + " to " + customer + ", ensuring all " + customer + " get an equal chance at success. At " + name + " we believe that our success is about putting " + customer + " first, and becuase we listen and deliver on their needs, we will always outperform companies that don't truly care. ";
-
+//benefit pargraph one
+var benefitParagraph1 = "August 6th," + name + " was named the top benefactor of the All Star Code 2016 Benefit." + lastName + "had donated a grand total of over" + netWorth "." + "Hosted at Christina Lewis Halpern’s summer home in the East Hamptons," + name + "is well known for their participation in various movements, including" + movement1 + ",and" + movement2 + "."
 
 // DONT PUT ANYTHING HERE!
 var hobbiesSubheading = '';
+
+subheading = subheading.capAllWord();
